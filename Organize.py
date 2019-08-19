db371769ba66f6093058a03746e98ab63f313d0b
D1360-64RC14 <gdiego416@gmail.com>
2019-08-18 23:30:54 -0300

    FOR LOOP alterado para MAP/FILTER

--- Organize.py
+++ Organize.py
@@ -1,29 +1,21 @@
 def log2json(log = str):
 
     log = log.split('\n')
-    
-    logFinal = {
-        "installed": [],
-        "upgraded": [],
-        "removed": [],
-        "full": []
-    }
-    
-    for line in log:
+
+    def organize(line):
+        output = []
         if '[ALPM] installed' in line:
             splitted = line.split()
-            dictString = {
+            output = list([{
                 'Date': splitted[0][1:] + ' ' + splitted[1][:-1],
                 'Action': splitted[3],
                 'Package': splitted[4],
                 'Version': splitted[5][1:-1]
-            }
-            logFinal["installed"].append(dictString)
-            logFinal["full"].append(dictString)
+            }])
 
         elif '[ALPM] upgraded' in line:
             splitted = line.split()
-            dictString = {
+            output = list([{
                 'Date': splitted[0][1:] + ' ' + splitted[1][:-1],
                 'Action': splitted[3],
                 'Package': splitted[4],
@@ -31,19 +23,27 @@ def log2json(log = str):
                     'older': splitted[5][1:],
                     'newer': splitted[7][:-1]
                 }
-            }
-            logFinal["upgraded"].append(dictString)
-            logFinal["full"].append(dictString)
+            }])
 
         elif '[ALPM] removed' in line:
             splitted = line.split()
-            dictString = {
+            output = list([{
                 'Date': splitted[0][1:] + ' ' + splitted[1][:-1],
                 'Action': splitted[3],
                 'Package': splitted[4],
                 'Version': splitted[5][1:-1]
-            }
-            logFinal["removed"].append(dictString)
-            logFinal["full"].append(dictString)
+            }])
 
-    return dict(logFinal)
+        return output
+
+    final = filter(organize, log)
+    final = list(map(organize, final))
+
+    logFinal = list([{
+        "installed": list(filter((lambda line: line[0]['Action'] == 'installed'), final)),
+        "upgraded": list(filter((lambda line: line[0]['Action'] == 'upgraded'), final)),
+        "removed": list(filter((lambda line: line[0]['Action'] == 'removed'), final)),
+        "full": final
+    }])
+
+    return list(logFinal)
